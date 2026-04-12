7419d1b43a6e9e708b58c37a634a73f742b98ed6
Ibarra <ibarrajames13@gmail.com>
2026-04-12 15:44:30 +0800

    CDDRRMO node fully working, deploy at anytime

--- main/get_message.cpp
+++ main/get_message.cpp
@@ -6,12 +6,15 @@
 
 int get_user_message(char *out_msg)
 {
+    // Read one line from the USB serial console
     if (fgets(out_msg, MAX_MSG_LEN, stdin) == NULL) {
         return 0;
     }
 
-    out_msg[strcspn(out_msg, "\n")] = '\0';
+    // Remove newline
+    out_msg[strcspn(out_msg, "\r\n")] = '\0';
 
+    // Ignore empty input
     if (out_msg[0] == '\0') {
         return 0;
     }
